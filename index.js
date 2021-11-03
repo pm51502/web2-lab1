729adf40a1e9ab1855d851e6075cf89dbf1b1ab2
Petar Milicevic <pm51502@fer.hr>
2021-11-03 02:07:41 +0100

    auth0 added

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const path = require("path");
+var fs = require("fs");
+var https = require("https");
 
+const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const { auth, requiresAuth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  idpLogout: true, //login not only from the app, but also from identity provider
+  secret: process.env.SECRET,
+  baseURL: `https://localhost:${port}`,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: "https://dev-3ifzacnj.us.auth0.com",
+  clientSecret: process.env.CLIENT_SECRET,
+  authorizationParams: {
+    response_type: "code",
+    //scope: "openid profile email"
+  },
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+//app.get("/", (req, res) => res.render("pages/index"));
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+if(process.env.port) {
+  https.createServer({
+        key: fs.readFileSync("server.key"),
+        cert: fs.readFileSync("server.cert"),
+      },
+        app).listen(port, function () {
+        console.log(`Server running at https://localhost:${port}/`);
+      });
+} else {
+  app.listen(port, () => console.log(`Listening on ${port}`));
+}
